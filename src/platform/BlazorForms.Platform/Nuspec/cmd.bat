18ba66f57a2e445660d5ff2ee9fe7ae9fbf547ec
Ev Uklad <euklad@gmail.com>
2022-12-11 12:25:29 +1100

    fixed rendering dependencies to allow webassembly support

--- src/platform/BlazorForms.Platform/Nuspec/cmd.bat
+++ src/platform/BlazorForms.Platform/Nuspec/cmd.bat
@@ -1,3 +1,4 @@
 ﻿cd C:\repos\BlazorForms\src\platform\BlazorForms.Platform\Nuspec
 nuget.exe pack BlazorForms.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 0.3.5
+nuget.exe pack BlazorForms.Rendering.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 0.3.5
 pause
