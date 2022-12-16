b2c4f180a6c0c6e378a4a80008ead06e4cb95705
Ev Uklad <euklad@gmail.com>
2022-12-17 09:59:59 +1100

    added all controls, but not tested yet

---
+++ src/rendering/BlazorForms.Rendering.MudBlazorUI/Components/DateEditControl.razor.js
@@ -0,0 +1,18 @@
+﻿export function blazorGetTimezoneOffset() {
+    return new Date().getTimezoneOffset();
+}
+export function blazorGetTimezone() {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
+}
+export function blazorNavigatorLanguage() {
+    return navigator.language;
+}
+export function blazorWindowLocationOrigin() {
+    return window.location.origin;
+}
+export function selectText(MyId) {
+    var element = document.querySelector("#" + MyId);
+    if (element.select) {
+        element.select();
+    }
+}
