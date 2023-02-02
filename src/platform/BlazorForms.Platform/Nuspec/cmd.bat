4b66bf2ec2aeed59a01d9ff18d1fa9eb709d169c
Ev Uklad <euklad@gmail.com>
2023-02-02 19:38:52 +1100

    changed nuspec

--- src/platform/BlazorForms.Platform/Nuspec/cmd.bat
+++ src/platform/BlazorForms.Platform/Nuspec/cmd.bat
@@ -1,5 +1,5 @@
 ﻿cd C:\repos\BlazorForms\src\platform\BlazorForms.Platform\Nuspec
-nuget.exe pack BlazorForms.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 0.8.2
+nuget.exe pack BlazorForms.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.8.2
 nuget.exe pack BlazorForms.Rendering.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 0.8.2
 
 cd C:\repos\BlazorForms\src\rendering\BlazorForms.Rendering.MudBlazorUI\
