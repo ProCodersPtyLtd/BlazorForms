f05750445f98662ba0c37b3ece3e66fc7b4ccd9e
Ev Uklad <euklad@gmail.com>
2023-02-14 13:47:18 +1100

    fixed diagrams

--- src/platform/BlazorForms.Platform/Nuspec/cmd.bat
+++ src/platform/BlazorForms.Platform/Nuspec/cmd.bat
@@ -3,7 +3,7 @@
 cd C:\repos\BlazorForms\src\platform\BlazorForms.Platform\Nuspec
 nuget.exe pack BlazorForms.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.8.3
 nuget.exe pack BlazorForms.Rendering.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.8.3
-nuget.exe pack BlazorForms.Rendering.Flows.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.9.2
+nuget.exe pack BlazorForms.Rendering.Flows.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.9.4
 
 rem cd C:\repos\BlazorForms\src\rendering\BlazorForms.Rendering.Flows\
 rem dotnet pack BlazorForms.Rendering.Flows.csproj --output  c:\temp\Nuget
