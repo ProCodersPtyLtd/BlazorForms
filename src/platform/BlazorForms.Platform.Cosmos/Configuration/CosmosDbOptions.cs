4eb079fe0a1feebe03ab2b6fcb6e84dd42f9515e
Mɨḫailo <michael@vkors.com>
2023-07-07 12:37:30 +1000

    Refactor Cosmos DB provider P1 #57

---
+++ src/platform/BlazorForms.Platform.Cosmos/Configuration/CosmosDbOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorForms.Platform.Cosmos.Configuration;
+
+public class CosmosDbOptions
+ {
+     public string Database { get; set; }
+     public string Uri { get; set; }
+     public string Key { get; set; }
+     public string EnvironmentTag { get; set; }
+     public string FlowCollection { get; set; }
+ }
