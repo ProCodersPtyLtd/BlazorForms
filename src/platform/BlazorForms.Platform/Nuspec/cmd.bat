1884deb5392b0e542247b352d7192d8aaa84d46e
Mɨḫailo <michael@vkors.com>
2023-07-12 09:58:49 +1000

    Refactor Cosmos DB provider P1 #57

--- src/platform/BlazorForms.Platform/Nuspec/cmd.bat
+++ src/platform/BlazorForms.Platform/Nuspec/cmd.bat
@@ -1,7 +1,7 @@
 rem
 
 cd C:\repos\BlazorForms\src\platform\BlazorForms.Platform\Nuspec
-nuget.exe pack BlazorForms.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.12.0
+nuget.exe pack BlazorForms.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.12.1
 nuget.exe pack BlazorForms.Shared.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.12.0
 nuget.exe pack BlazorForms.Rendering.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.12.0
 nuget.exe pack BlazorForms.Rendering.Flows.nuspec -NonInteractive -OutputDirectory c:\temp\Nuget -Verbosity Detailed -version 1.12.0
